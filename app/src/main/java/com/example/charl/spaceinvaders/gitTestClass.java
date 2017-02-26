326165cef1f5eb6760d3091f5c0866e61d5c3bce
pablogarciabenayas <pablogarciabenayas@gmail.com>
2017-02-26 12:13:55 +0100

    Nueva rama de prueba, contiene una clase de ejemplo que no afecta a la funcionalidad original de la aplicacion.

---
+++ app/src/main/java/com/example/charl/spaceinvaders/gitTestClass.java
@@ -0,0 +1,22 @@
+package com.example.charl.spaceinvaders;
+
+/**
+ * Created by pablo on 26/02/17.
+ */
+
+public class gitTestClass {
+
+    private int attributeX;
+    private int attributeY;
+
+
+    private gitTestClass(int x, int y){
+        this.attributeX = x;
+        this.attributeY = y;
+    }
+
+    private String testMethod(){
+        return "Esto es una clase de prueba para crear una nueva rama en git, " +
+                "el valor de x es: "+ this.attributeX +" y el valor de y: " +this.attributeY;
+    }
+}
